40d3d8f85ca4982701c23de8a2c118e36e5ed32b
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-05 16:09:30 +0300

    Added more methods to query the db

--- backend/src/services/song.service.ts
+++ backend/src/services/song.service.ts
@@ -16,10 +16,38 @@ export class SongService {
         return await this.songRepository.find();
       }
       async findAllByBand(): Promise<Song[]> {
-        return await this.songRepository.find({ order: { band: 'ASC' } }); // Get the songs ordered by the Band name.
+        return await this.songRepository.find({ 
+            order: { band: 'ASC' } // Get the songs ordered by the Band name.
+        }); 
       }
-
+      async findSongsByBand(bandName: string): Promise<Song[]> {
+        return await this.songRepository.find({ 
+            where : { band: bandName},// Get All the songs of this band
+            order: { year: 'ASC' } // Order by the year.
+        }); 
+      }
+      async findSongsByYear(year: number): Promise<Song[]> {
+        return await this.songRepository.find({ 
+            where : { year: year},// Get All the songs from the same year that was passed
+            order: { band: 'ASC' } // Ordered by the Band name.
+        }); 
+      }
+      async findSpecificSong(songName: string, bandName: string): Promise<Song[]> {
+        return await this.songRepository.find({ 
+            where : { 
+                songName: songName, 
+                band: bandName 
+            }
+        }); 
+      }
       async addSong(song: Song): Promise<Song> {
         return await this.songRepository.save(song);
       }
+      async deleteSongByNameAndBand(songName: string, bandName: string): Promise<void> {
+        await this.songRepository.delete({ 
+            songName: songName, 
+            band: bandName 
+        });
+      }
+      
   }
