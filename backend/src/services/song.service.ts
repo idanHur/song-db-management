9e23eea2e02793464a626502f8e856f95d33fd32
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-05 15:46:17 +0300

    Added constructor to SongService

--- backend/src/services/song.service.ts
+++ backend/src/services/song.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Song } from '../entities/song.entity';
 
 @Injectable()
 export class SongService {
+    constructor(
+        @InjectRepository(Song)
+          /* songRepository is the instance of the 
+             Repository to handle DB operations for the Song entity. */
+        private songRepository: Repository<Song>,
+      ) {}
 
   }
