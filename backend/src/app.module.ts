3969209222b9a49cb4bcd85517fc25f1c605c338
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-05 14:09:12 +0300

    Added connection to mysql using TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database', // Name of the service in docker-compose.yml
+      port: 3306,
+      username: 'user', 
+      password: 'password',
+      database: 'songdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
