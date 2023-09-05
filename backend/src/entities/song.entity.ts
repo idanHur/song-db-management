55dedbe092c0ca448e6602cd1e1ef1ad75b40a63
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-05 17:39:31 +0300

    Added class-validator functionality

--- backend/src/entities/song.entity.ts
+++ backend/src/entities/song.entity.ts
@@ -1,17 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+import { IsString, IsNotEmpty, IsInt, Min, Max } from 'class-validator';
 
 @Entity()
 @Unique(["songName", "band", "year"])
 export class Song {
   @PrimaryGeneratedColumn()
-  id: number;
+  id?: number;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   songName: string;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   band: string;
   
   @Column()
+  @IsInt()
+  @Min(1900)  // Minimum year constraint
+  @Max(new Date().getFullYear())  // Maximum year constraint
   year: number;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // This line enables global validation
   await app.listen(3000);
 }
 bootstrap();
