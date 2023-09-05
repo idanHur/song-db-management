e103f19159c95297f81b34fe97d7d59405b6f231
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-05 22:08:05 +0300

    Added get method for the diffrent endpoints

--- frontend/src/services/songService.ts
+++ frontend/src/services/songService.ts
@@ -16,7 +16,37 @@ export const getAllSongs = async (): Promise<Song[]> => {
     return response.json();
   };
 
-
+  export const getAllSongsOrderByBand = async (): Promise<Song[]> => {
+    const response = await fetch(`${BASE_URL}/orderByBand`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch songs ordered by band.');
+    }
+    return response.json();
+  };
+  
+  export const getSongsByBand = async (bandName: string): Promise<Song[]> => {
+    const response = await fetch(`${BASE_URL}/byBand/${bandName}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch songs by ${bandName}.`);
+    }
+    return response.json();
+  };
+  
+  export const getSongsByYear = async (year: number): Promise<Song[]> => {
+    const response = await fetch(`${BASE_URL}/byYear/${year}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch songs from ${year}.`);
+    }
+    return response.json();
+  };
+  
+  export const getSpecificSong = async (songName: string, band: string): Promise<Song[]> => {
+    const response = await fetch(`${BASE_URL}/specificSong?songName=${encodeURIComponent(songName)}&band=${encodeURIComponent(band)}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch the specific song ${songName} by ${band}.`);
+    }
+    return response.json();
+  };
 
 
 
