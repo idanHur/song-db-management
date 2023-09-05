3c4514da78d57a8c0abb46da7b76947529371b25
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-05 21:40:20 +0300

    Created SongsTable.tsx

---
+++ frontend/src/components/SongsTable.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Song {
+  id: number;
+  bandName: string;
+  songName: string;
+  year: number;
+}
+
+const SongsTable: React.FC = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default SongsTable;
