08b2afacdf1ec0a1a02d036c405dddd7a99b78a3
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-09-05 22:16:55 +0300

    Added add and delete endpoint function

--- frontend/src/services/songService.ts
+++ frontend/src/services/songService.ts
@@ -1,3 +1,5 @@
+import { json } from "stream/consumers";
+
 interface Song {
     id?: number
     bandName: string;
@@ -16,40 +18,65 @@ export const getAllSongs = async (): Promise<Song[]> => {
     return response.json();
   };
 
-  export const getAllSongsOrderByBand = async (): Promise<Song[]> => {
-    const response = await fetch(`${BASE_URL}/orderByBand`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch songs ordered by band.');
+export const getAllSongsOrderByBand = async (): Promise<Song[]> => {
+const response = await fetch(`${BASE_URL}/orderByBand`);
+if (!response.ok) {
+    throw new Error('Failed to fetch songs ordered by band.');
+}
+return response.json();
+};
+
+export const getSongsByBand = async (bandName: string): Promise<Song[]> => {
+const response = await fetch(`${BASE_URL}/byBand/${bandName}`);
+if (!response.ok) {
+    throw new Error(`Failed to fetch songs by ${bandName}.`);
+}
+return response.json();
+};
+
+export const getSongsByYear = async (year: number): Promise<Song[]> => {
+const response = await fetch(`${BASE_URL}/byYear/${year}`);
+if (!response.ok) {
+    throw new Error(`Failed to fetch songs from ${year}.`);
+}
+return response.json();
+};
+
+export const getSpecificSong = async (songName: string, band: string): Promise<Song[]> => {
+const response = await fetch(`${BASE_URL}/specificSong?songName=${encodeURIComponent(songName)}&band=${encodeURIComponent(band)}`);
+if (!response.ok) {
+    throw new Error(`Failed to fetch the specific song ${songName} by ${band}.`);
+}
+return response.json();
+};
+
+export const addSong = async (song: Song): Promise<Song> => {
+    const response = await fetch(BASE_URL,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(song),
+    });
+    if(!response.ok){
+        throw new Error('Failed to add song.');
     }
     return response.json();
-  };
-  
-  export const getSongsByBand = async (bandName: string): Promise<Song[]> => {
-    const response = await fetch(`${BASE_URL}/byBand/${bandName}`);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch songs by ${bandName}.`);
+};
+export const deleteSong  = async (songName: string, band: string): Promise<void> => {
+    const response = await fetch(BASE_URL,{
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({songName, band}),
+    });
+    if(!response.ok){
+        throw new Error(`Failed to delete song ${songName} by ${band}.`);
     }
-    return response.json();
-  };
-  
-  export const getSongsByYear = async (year: number): Promise<Song[]> => {
-    const response = await fetch(`${BASE_URL}/byYear/${year}`);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch songs from ${year}.`);
-    }
-    return response.json();
-  };
-  
-  export const getSpecificSong = async (songName: string, band: string): Promise<Song[]> => {
-    const response = await fetch(`${BASE_URL}/specificSong?songName=${encodeURIComponent(songName)}&band=${encodeURIComponent(band)}`);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch the specific song ${songName} by ${band}.`);
-    }
-    return response.json();
-  };
+};
 
 
 
 
-
-  export {};
+export {};
